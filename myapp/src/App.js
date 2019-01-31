cc1070146e3f026824a06d9505353f436b15d7b8
coderzero8 <coderzero8@gmail.com>
2019-01-31 09:46:18 +0800

    react skeleton

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,39 +1,10 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninjas'
-import AddNinja from './AddNinja'
 // class based component
 class App extends Component {
   state = {
-    ninjas: [
-      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    array1: [
+    
     ]
-
-
-  }
-
-  fn_addninja = (ninja) => { //(ninja) is the parameter passed.
-      console.log(ninja)
-      ninja.id = Math.random();
-
-      let copyninjas = [...this.state.ninjas,ninja] // .. is a spread operator
-
-      this.setState({
-          // ninja:this.ninjas.push(ninja) bad practice 
-          // copy the state and push then re-set all i think.
-          ninjas:copyninjas
-      })
-  }
-
-  fn_deleteNinja = (id) => {
-    let new_arr_ninja_out = this.state.ninjas.filter( new_array_ninja_in => {
-      return new_array_ninja_in.id !==id
-    })
-    this.setState({
-      ninjas:new_arr_ninja_out
-    })
-    console.log(id)
   }
 
   componentDidMount(){ //hooks component method default
@@ -47,9 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My first React app</h1>
-        <Ninjas prop_deleteninja={this.fn_deleteNinja} ninjas={this.state.ninjas}  />
-        <AddNinja addNinja={this.fn_addninja}/>
+
       </div>
     );
   }
