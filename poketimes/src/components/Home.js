f1837927a6ca3a41b5d7228c39ab6410dd7e53b3
coderzero8 <coderzero8@gmail.com>
2019-01-31 14:09:17 +0800

    finishe tut33

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,13 +1,14 @@
 import React ,{Component}from 'react'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
-
+import Pokeball from '../pokeball.png'
 class Home extends Component{
   state = {
     posts:[]
   }
   componentDidMount(){
       
+      // axios.get('https://jsonplaceholder.typicode.com/posts')
       axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res=>{
             console.log(res)
@@ -24,10 +25,11 @@ class Home extends Component{
         posts.map(post=>{
             return(
               <div className="post card " key={post.id}>
+                  <img src={Pokeball} alt="A pokeball"/>
                   <div className="card-content">
-                    
+
                     <Link to={'/'+post.id}>
-                      <span className='card-title'>{post.title}</span>
+                      <span className='card-title red-text'>{post.title}</span>
                     </Link>
                   
                       <p>{post.body}</p>
@@ -42,7 +44,7 @@ class Home extends Component{
       )
 
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postsList}
       </div>
